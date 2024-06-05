c2d8c4b5db69da64dc51843e09ba7bb405d5c241
Priyansh Lohiya <priyanshlohiya2002@gmail.com>
2024-06-06 01:35:04 +0530

    Modular changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>BlueSky</span>
-            <span className='text-slate-700'>ESTATE</span>
+            <span className='text-slate-500'>RealEstate</span>
+            {/* <span className='text-slate-700'></span> */}
           </h1>
         </Link>
         <form
